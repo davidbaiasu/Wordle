df89b2910bc3e1d23546b82833835b39bc805028
davidbaiasu <baiasudavidalin@gmail.com>
2025-11-21 00:02:02 +0200

    Added insert and delete letter

--- wordlescript.js
+++ wordlescript.js
@@ -1,3 +1,5 @@
+const cells = document.querySelectorAll("#div_table td");
+
 let words_database = ["SLOPE","NOISE","CRANE"];
 
 let words_matrix = [
@@ -21,11 +23,35 @@ document.addEventListener('keydown', (event) => {
 	}
 	
 	else if( event.key === 'Backspace' ){
-		console.log("backspace :D");
+		
+		if( current_letter != 0 ){
+			
+			current_letter--;
+			
+			const cellID = `cell_${current_word}_${current_letter}`;
+			const cell = document.getElementById(cellID);
+			cell.textContent = '';
+			
+		}
+		
 	}
 
 	else if ( event.key >= 'a' && event.key <= 'z' ) {
-		console.log(`You pressed the letter: ${event.key}`);
+		
+		const new_letter = event.key.toUpperCase();
+		
+		words_matrix[current_word][current_letter] = new_letter;
+		
+		const cellID = `cell_${current_word}_${current_letter}`;
+		const cell = document.getElementById(cellID);
+        
+		cell.textContent = new_letter;
+		
+		current_letter++;
+		if(current_letter > 4){
+			current_word++;
+			current_letter = 0;
+		}
 	}
    
 });
