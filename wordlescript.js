411d36d1ca38c4ff65bf953caa798c417bc9f761
davidbaiasu <baiasudavidalin@gmail.com>
2025-11-21 12:20:55 +0200

    Cells turn green when correct letter is introduced

--- wordlescript.js
+++ wordlescript.js
@@ -15,15 +15,30 @@ let words_matrix = [
 
 let current_word   = 0;
 let current_letter = 0;
+const MAX_WORD_LENGTH = 4;
+let GAME_OVER = false;
+
+const solution = words_database[Math.floor(Math.random() * words_database.length)];
+console.log(solution)
 
 document.addEventListener('keydown', (event) => {
 	
-	if( event.key === 'Enter' ){
+	if( event.key === 'Enter' && current_letter > MAX_WORD_LENGTH ){
+	
+		for( let i = 0; i <= MAX_WORD_LENGTH; i++ ){
+			
+			if( solution[i] == words_matrix[current_word][i] ){
+			
+				const cellID = `cell_${current_word}_${i}`;
+				const cell = document.getElementById(cellID);
+				cell.style.backgroundColor = "lightgreen";
+			
+			}
 		
-		if( current_letter > 4 ){
-			current_letter = 0;
-			current_word++;
 		}
+	
+		current_letter = 0;
+		current_word++;	
 		
 	}
 	
@@ -43,7 +58,7 @@ document.addEventListener('keydown', (event) => {
 
 	else if ( event.key >= 'a' && event.key <= 'z' ) {
 		
-		if(current_letter < 5){
+		if( current_letter <= MAX_WORD_LENGTH ){
 			
 			const new_letter = event.key.toUpperCase();
 		
